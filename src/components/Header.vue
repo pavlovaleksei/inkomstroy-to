af4ef8af229cc81fec3abfc0d702848fc1fc07f9
Pavlov Aleksey <a.pavlov@alfatell.ru>
2020-11-27 11:48:33 +0400

    Replace header image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,20 +12,20 @@
                 </div>
                 <div>
                     <div class="containerPage">
-                        <div style="color: white; text-align: left"><b>ООО "Инкомстрой-ТО"</b></div>
-                        <div style="display: flex; color: white;">
+                        <div style="color: black; text-align: left"><b>ООО "Инкомстрой-ТО"</b></div>
+                        <div style="display: flex; color: black;">
                             <div style="">Адрес:</div>
                             <div style="padding-left: 1%">410036, Россия, г. Саратов, ул. Ново-Астраханское шоссе, д. 73</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">Рабочий телефон:</div>
                             <div style="padding-left: 1%">+7 8452 94-05-56</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">E-mail:</div>
                             <div style="padding-left: 1%">inkomto@yandex.ru</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">График работы:</div>
                             <div style="padding-left: 1%; text-align: left">пн-пт: 08.30 - 17.00<br>сб: 09.00 - 13.00<br>вс: выходной<br>обед: 13.00 - 14.00</div>
                         </div>
@@ -38,20 +38,20 @@
                 <img class="header_image" alt="Photo" src="../assets/logotype.png">
                 <div>
                     <div class="containerPage">
-                        <div style="color: white; text-align: left"><b>ООО "Инкомстрой-ТО"</b></div>
-                        <div style="display: flex; color: white;">
+                        <div style="color: black; text-align: left"><b>ООО "Инкомстрой-ТО"</b></div>
+                        <div style="display: flex; color: black;">
                             <div style="">Адрес:</div>
-                            <div style="padding-left: 1%">410036, Россия, г. Саратов, ул. Ново-Астраханское шоссе, д. 73</div>
+                            <div style="padding-left: 1%; text-align: left">410036, Россия, г. Саратов, <br>ул. Ново-Астраханское шоссе, д. 73</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">Рабочий телефон:</div>
                             <div style="padding-left: 1%">+7 8452 94-05-56</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">E-mail:</div>
                             <div style="padding-left: 1%">inkomto@yandex.ru</div>
                         </div>
-                        <div style="display: flex; color: white;">
+                        <div style="display: flex; color: black;">
                             <div style="">График работы:</div>
                             <div style="padding-left: 1%; text-align: left">пн-пт: 08.30 - 17.00<br>сб: 09.00 - 13.00<br>вс: выходной<br>обед: 13.00 - 14.00</div>
                         </div>
